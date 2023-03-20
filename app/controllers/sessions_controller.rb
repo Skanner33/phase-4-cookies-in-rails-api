110df15291e13e817fb299fa35bab48709ec170f
Skanner33 <mathewmageto@gmail.com>
2023-03-20 08:46:48 +0300

    completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   
-  def index
+  def indexz
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
